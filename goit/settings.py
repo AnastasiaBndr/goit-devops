4b6e008ab189ec57174519037f7f005bf8782910
AnastasiaBndr <abondarchuk1149@gmail.com>
2026-06-12 10:38:57 +0000

    Add Dockerized Django project with PostgreSQL and Nginx

--- goit/settings.py
+++ goit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,3 +121,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATABASES = {
+
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+		'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+		'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+		'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+		'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+	}
+}
